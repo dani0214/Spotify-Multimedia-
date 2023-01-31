33bedc17d395619b18b40ffa6a47aab08db6bf6b
dani0214 <daniddf@live.com>
2023-01-31 12:24:43 +0100

    Primer Commit

---
+++ app/src/main/java/com/example/sporify/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sporify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button registro;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login=findViewById(R.id.btLogin);
+        registro=findViewById(R.id.btRegistrar);
+
+    }
+}
